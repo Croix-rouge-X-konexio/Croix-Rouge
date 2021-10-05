18faa8d7d32c5766775a889e4e2a68ac8391213c
Eliott17 <poulaineliott@gmail.com>
2021-10-05 14:42:22 +0200

    first commit

---
+++ Schema.js
@@ -0,0 +1,164 @@
+const mongoose = require("mongoose");
+
+const UsersSchema = new mongoose.Schema({
+    firstName: {
+        required: true,
+        type: String,
+    },
+    lastName: {
+        required: true,
+        type: String,
+    },
+    email: {
+        required: true,
+        type: String,
+    },
+    password: {
+        required: true,
+        type: String,
+    },
+    phoneNumber: {
+        required: true,
+        type: Number,
+    },
+    area: {
+        required: true,
+        type: String,
+    },
+    category: {
+        required: true,
+        type: String,
+    },
+    // picture: {
+    //     required: false,
+    //     type: File
+    // },
+    isAdmin: {
+        required: true,
+        type: Boolean,
+    },
+    isValidate: {
+        required: true,
+        type: Boolean
+    }
+});
+
+const UsersExperienceSchema = new mongoose.Schema({
+    title: {
+        required: true,
+        type: String,
+    },
+    startingDate: {
+        required: true,
+        type: Number,
+    },
+    endingDate: {
+        required: true,
+        type: Number,
+    },
+    userId: [{
+        type: mongoose.Types.ObjectId,
+        ref: "Users", // à vérif
+    }],
+});
+
+const UsersEducationSchema = new mongoose.Schema({
+    EducationId: [{
+        type: mongoose.Types.ObjectId,
+        ref: "EducationList",
+    }],
+    date: {
+        required: true,
+        type: Number,
+    },
+    // certificate: {
+    //     required: true,
+    //     type: fichier
+    // },
+    userId: [{
+        type: mongoose.Types.ObjectId,
+        ref: "Users", // à vérif
+    }],
+});
+
+const EventsSchema = new mongoose.Schema({
+    title: {
+        required: true,
+        type: String,
+    },
+    date: {
+        required: true,
+        type: Number,
+    },
+    heure: {
+        required: true,
+        type: Number,
+    },
+    duration: {
+        required: true,
+        type: Number,
+    },
+    place: {
+        required: true,
+        type: String,
+    },
+    // picture: {
+    //     required: true,
+    //     type: File
+    // },
+    description: {
+        required: true,
+        type: String,
+    },
+    userId: [{
+        type: mongoose.Types.ObjectId,
+        ref: "Users", // à vérif
+    }],
+});
+
+const EventEducatoinRelatedSchema = new mongoose.Schema({
+    EventId: [{
+        type: mongoose.Types.ObjectId,
+        ref: "Event", // à vérif
+    }],
+    EducationId: [{
+        type: mongoose.Types.ObjectId,
+        ref: "EducationList",
+    }],
+});
+
+const EducationListSchema = new mongoose.Schema({
+    EducationName: {
+        required: true,
+        type: String,
+    },
+});
+
+const EventAttendeesSchema = new mongoose.Schema({
+    EventId: [{
+        type: mongoose.Types.ObjectId,
+        ref: "Event", // à vérif
+    }],
+    userId: [{
+        type: mongoose.Types.ObjectId,
+        ref: "Users", // à vérif
+    }],
+});
+
+const User = mongoose.model("User", UsersSchema);
+const UserExperience = mongoose.model("UserExperience", UsersExperienceSchema);
+const UserEducation = mongoose.model("UserEducation", UsersEducationSchema);
+const Event = mongoose.model("Event", EventsSchema);
+const EventEducatoinRelated = mongoose.model("EventEducatoinRelated", EventEducatoinRelatedSchema);
+const EducationList = mongoose.model("EducationList", EducationListSchema);
+const EventAttendees = mongoose.model("EventAttendees", EventAttendeesSchema);
+
+module.exports = {
+    User,
+    UserExperience,
+    UserEducation,
+    Event,
+    EventEducatoinRelated,
+    EducationList,
+    EventAttendees,
+};
---
+++ server.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+
+// Shema
+const Schema = require("./Schema");
+// console.log(Schema);
+
+// dotenv config
+dotenv.config(
+    { path: "./congif.env" }
+);
+
+// Mongoose Connect
+mongoose
+    .connect(process.env.DB, {
+        useNewUrlParser: true,
+    })
+    .then(() => {
+        console.log("Connected to MongoDB !");
+    });
+
+// Middleware
+app.use(express.json());
+
+app.post("/user", async (req, res) => {
+    const User = req.body
+    const hashedPassword = await bcrypt.hash(User.password, 12);
+    const educationExist = await Schema.EducationList.findOne({
+        education: User.education,
+    });
+    // remplir l'EducationList
+    try {
+        const newUser = await Schema.User.create({
+            firstName: User.firstName,
+            lastName: User.lastName,
+            phoneNumber: User.phoneNumber,
+            email: User.email,
+            area: User.area,
+            category: User.category,
+            password: hashedPassword,
+            isAdmin: false,
+            isValidate: false,
+            // picture
+        });
+        // console.log(newUser.id);
+        await Schema.UserEducation.create({
+            date: User.date,
+            userId: newUser.id,
+            EducationId: educationExist.id,
+            // certificate
+        });
+        console.log()
+        await Schema.UserExperience.create({
+            title: User.title,
+            startingDate: User.startingDate,
+            endingDate: User.endingDate,
+            userId: newUser.id,
+        });
+        res.status(201).json({
+            message: `New user added ! ${User.email}`,
+            data: User
+        });
+    }
+    catch (err) {
+        return res.json({
+            message: "error",
+        });
+    }
+});
+
+// LISTEN
+app.listen(process.env.PORT, () => {
+    console.log("Server listening on port: " + process.env.PORT);
+});
+
+// exemple postman
+// {
+//     "firstName": "User.firstName",  
+//     "lastName": "User.lastName",
+//     "phoneNumber": 6565,
+//     "email": "User.email",
+//     "area": "User.area",
+//     "category": "User.category",
+//     "password": "hashedPassword",
+//     "date": 654654,
+//     "title": "User.title",
+//     "startingDate": 54654,
+//     "endingDat": 656,
+//     "education": "ambulancier"
+// }
