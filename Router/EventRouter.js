48fc72b8eb23527569ec1dd1c8ffafadf09f7419
Eliott17 <poulaineliott@gmail.com>
2021-10-11 14:15:17 +0200

    Event Router finish and listRouter finish

--- Router/EventRouter.js
+++ Router/EventRouter.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.get("/", Protect.protect, EventController.getAllEvent); // V
 router.post("/", Protect.protect, isAdmin.isAdmin, EventController.addEvent); // V
-router.delete("/:eventId", Protect.protect, isAdmin.isAdmin, EventController.deleteEvent); // X
+router.delete("/:eventId", Protect.protect, isAdmin.isAdmin, EventController.deleteEvent); //  Récupérer coté front l'id de l'Event
 // router.patch("/", Protect.protect, isAdmin.isAdmin, EventController.patchEvent); // X
 
 module.exports = router;
--- Router/ListRouter.js
+++ Router/ListRouter.js
@@ -7,7 +7,7 @@ const checkId = require("../middleware/checkId");
 
 router.get("/listUsers", Protect.protect, isAdmin.isAdmin, ListController.getAllUsers); // V
 router.get("/listUsers/:userid", Protect.protect, isAdmin.isAdmin, checkId.checkId, ListController.getOneUser); // X on attend la PimpMy card user
-router.patch("/listUsers/:userid", Protect.protect, isAdmin.isAdmin, checkId.checkId, ListController.modifyOneUser); // X si on a le temps et que tout est nickel
-router.delete("/listUsers/:userid", Protect.protect, isAdmin.isAdmin, checkId.checkId, ListController.deleteOneUser); // X on attend la PimpMy card user
+router.delete("/listUsers/:userid", Protect.protect, isAdmin.isAdmin, checkId.checkId, ListController.deleteOneUser); // V on attend la PimpMy card user / de pouvoir récup l'id
+router.patch("/listUsers/:userid", Protect.protect, isAdmin.isAdmin, checkId.checkId, ListController.modifyOneUser); // V on attend la PimpMy card user / de pouvoir récup l'id
 
 module.exports = router;
